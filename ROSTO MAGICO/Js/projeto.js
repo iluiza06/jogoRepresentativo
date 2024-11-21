d46e109c2b9ffb13de70e9a7e8b16927b5214653
Isadora <134619001+iluiza06@users.noreply.github.com>
2024-11-21 14:09:00 -0300

    Adicionando arquivos do jogo

--- ROSTO MAGICO/Js/projeto.js
+++ ROSTO MAGICO/Js/projeto.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const imagens = [
     'IMG/card_olho.png',
     'IMG/card_nariz.png',
@@ -49,3 +50,56 @@ function tocarSom(botaoId) {
         verificarCorreto('bouvido');
     });
 
+=======
+const imagens = [
+    'IMG/card_olho.png',
+    'IMG/card_nariz.png',
+    'IMG/card_boca.png',
+    'IMG/card_ouvido.png'  
+];
+
+let indiceAtual = 0;
+
+const botoes = {
+    'bolho': 0,  
+    'bnariz': 1, 
+    'bboca': 2,  
+    'bouvido': 3 
+};
+
+function mudarImagem() {
+    
+    indiceAtual = (indiceAtual + 1) % imagens.length;;
+    document.getElementById('imagem_trocada').style.backgroundImage = `url(${imagens[indiceAtual]})`;
+}
+
+function verificarCorreto(botaoId) {
+    const indiceBotao = botoes[botaoId]; 
+    if (indiceBotao === indiceAtual) { 
+        mudarImagem();  
+    }
+}
+
+function tocarSom(botaoId) {
+    const som = document.getElementById(`audio_${botaoId}`);
+    som.play();
+}
+
+    document.getElementById('bolho').addEventListener('click', function(){
+        tocarSom('olho');
+        verificarCorreto('bolho');
+      });
+      document.getElementById('bnariz').addEventListener('click', function(){
+        tocarSom('nariz');
+        verificarCorreto('bnariz');
+      });
+      document.getElementById('bboca').addEventListener('click', function(){
+        tocarSom('boca');
+        verificarCorreto('bboca');
+      });
+      document.getElementById('bouvido').addEventListener('click', function(){
+        tocarSom('ouvido');
+        verificarCorreto('bouvido');
+    });
+
+>>>>>>> d586554 (Adicionando arquivos do jogo)
